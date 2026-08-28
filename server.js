6755e1480ecfeadb93cdca187469bfabc801f76e
Lihi <lihi.az22@gmail.com>
2026-08-28 11:27:02 +0300

    jkbxh

--- server.js
+++ server.js
@@ -23,11 +23,10 @@ app.use(session({
 // Connect to MongoDB using the environment variable from .env
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/socialNetworkDB';
 
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(MONGO_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
+
 
 // Routes
 const authRoutes = require('./routes/authRoutes');

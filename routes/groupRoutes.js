ec0f26a98639c8acd2efb5df1368396ce8b97ffd
Lihi <lihi.az22@gmail.com>
2026-08-28 11:23:43 +0300

    fix

---
+++ routes/groupRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// כאן תוכלי להוסיף בהמשך את הראוטים של הקבוצות, למשל:
+// router.get('/', groupController.getGroups);
+
+module.exports = router;
